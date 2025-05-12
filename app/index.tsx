eb136c2522e5980c7db38bac55d0918cdb8599a7
suo3 <samuel.o.asare@gmail.com>
2025-05-12 19:45:57 -0400

    add tabs folder

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/about" style={styles.button}>Go to About Screen</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff'
+  },
+  button: {
+    fontSize:20,
+    textDecorationLine: 'underline',
+    color: '#fff'
+  }
+})
