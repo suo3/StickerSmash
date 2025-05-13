09f55d90cc3ca13f2cfcf5f5de312a79becea8b6
suo3 <samuel.o.asare@gmail.com>
2025-05-13 19:51:13 -0400

    add image picker screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,20 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Button from "../components/Button";
+import ImageViewer from "../components/ImageViewer";
 
+const PlaceholderImage = require('@/assets/images/background-image.png')
 export default function Index() {
   return (
     <View
       style={styles.container}
     >
-      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/about" style={styles.button}>Go to About Screen</Link>
+      <View style={styles.imageContainer}>
+         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -19,12 +26,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    color: '#fff'
+  imageContainer:{
+    flex:1,
   },
-  button: {
-    fontSize:20,
-    textDecorationLine: 'underline',
-    color: '#fff'
+  footerContainer:{
+    flex: 1/3,
+    alignItems: 'center'
   }
 })
