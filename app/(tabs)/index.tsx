639e3392bb9306c7b8c6b685b001a0f323e0ffac
suo3 <samuel.o.asare@gmail.com>
2025-05-20 19:29:49 -0400

    add domtoimage for web view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
+import domtoimage from 'dom-to-image';
 import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
 import { useRef, useState } from "react";
-import { ImageSourcePropType, StyleSheet, View } from "react-native";
+import { ImageSourcePropType, Platform, StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { captureRef } from 'react-native-view-shot';
 import Button from "../components/Button";
@@ -59,7 +60,8 @@ export default function Index() {
 
 
  const onSaveImageAsync = async () => {
-    try {
+  if (Platform.OS !== 'web') {
+      try {
       const localUri = await captureRef(imageRef, {
         height: 440,
         quality: 1,
@@ -71,6 +73,23 @@ export default function Index() {
       }
     } catch (e) {
       console.log(e);
+    }
+  }  else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
 
